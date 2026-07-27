7e649e06b73faecf7d3a4586a7420ac365a93fad
Stream <Stream_2@qq.com>
2026-07-27 17:07:38 +0800

    feat: PathAsStringSerializer

---
+++ utils/kotlinx-io-serialization/src/commonMain/kotlin/io/github/stream29/codex/lite/utils/kotlinxioserialization/PathAsStringSerializer.kt
@@ -0,0 +1,22 @@
+package io.github.stream29.codex.lite.utils.kotlinxioserialization
+
+import kotlinx.io.files.Path
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+/** Serializes a [Path] through its platform-independent string representation. */
+public object PathAsStringSerializer : KSerializer<Path> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("kotlinx.io.files.Path", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: Path) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): Path =
+        Path(decoder.decodeString())
+}
