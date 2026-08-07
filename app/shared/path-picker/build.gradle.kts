c45f6733b478464772cc943ee945916617236a5d
Stream <Stream_2@qq.com>
2026-08-07 23:38:19 +0800

    fix: path picker at home

--- app/shared/path-picker/build.gradle.kts
+++ app/shared/path-picker/build.gradle.kts
@@ -6,6 +6,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             api(project(":utils-kotlinx-io-coroutines"))
+            implementation(project(":utils-os-environment"))
         }
     }
 }
