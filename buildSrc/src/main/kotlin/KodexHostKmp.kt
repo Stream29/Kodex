b902394e2c334a59d19e3f2193edf8adca4e92e4
Stream <Stream_2@qq.com>
2026-09-01 07:17:37 +0800

    chore: bump version

--- buildSrc/src/main/kotlin/KodexHostKmp.kt
+++ buildSrc/src/main/kotlin/KodexHostKmp.kt
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinHierarchyTemplate
 
 internal fun Project.configureCoordinates() {
     group = "io.github.stream29"
-    version = "0.3.0"
+    version = "0.3.1"
 }
 
 internal fun KotlinMultiplatformExtension.configureCompiler() {
--- buildSrc/src/main/kotlin/kodex.kmp-shared.gradle.kts
+++ buildSrc/src/main/kotlin/kodex.kmp-shared.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "io.github.stream29"
-version = "0.3.0"
+version = "0.3.1"
 
 kotlin {
     explicitApi()
